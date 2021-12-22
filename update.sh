a0e3929c432d21d6afd264ce5bf164ec4e0c3a9a
James McFarland <24841376+jamesmcfarland@users.noreply.github.com>
2021-12-22 21:49:10 +0000

    fixed update.sh because i am silly

--- update.sh
+++ update.sh
@@ -2,8 +2,9 @@
 git reset --hard
 git gc
 git pull
+chmod a+x update.sh
 dotnet publish -c Release
 cd bin/Release/net6.0
 pm2 stop "dotnet qcs-whitelist"
 pm2 delete "dotnet qcs-whitelist"
-pm2 start "dotnet qcs-whitelist"
+pm2 start "dotnet qcs-whitelist.dll"
